1b65b028f09f423910f5007f17bf9f1186332856
cezarpm <cezar_pm@hotmail.com>
2022-08-24 01:51:56 -0300

    exercicios aula3

---
+++ aula3/ex2.py
@@ -0,0 +1,9 @@
+valor1 = int(input("Digite um valor: "))
+
+valor2 = int(input("Digite um valor: "))
+
+if(valor1 < valor2):
+    print(f"Valor 1 é menor que valor 2 => {valor1} < {valor2}")
+
+if(valor1 > valor2):
+    print(f"Valor 2 é menor que valor 1 => {valor2} < {valor1}")
